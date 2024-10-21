2addfa81caa61ed61687b5498e710f04391e3ce3
DARIUS ZEKPA <dariuszekpa@gmail.com>
2024-10-21 01:51:47 +0100

    REA

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/home/'
-import Survey from './pages/Survely'
-import Results from './pages/Results/'
-import Freelances from './pages/Freelances/'
-import Error from './pages/Errors.tsx'
-
-
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/';
+import Survey from './pages/Survely';
+import Results from './pages/Results/';
+import Freelances from './pages/Freelances/';
+import Error from '@/components/Errors/';
+import './index.css';
+import Header from './pages/Header/index.tsx';
+ 
+import { ThemeProvider } from '@/utils/context'
+import Footer from './pages/Footer/index.tsx';
+import GlobalStyle from './utils/style/GlobaleStyle.tsx';
 
 createRoot(document.getElementById('root')!).render(
      <StrictMode>
           <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/survey" element={<Survey />} />
-                <Route path="/survey/:questionNumber" element={<Survey />} />
-               <Route path="/results" element={<Results />} />
-               <Route path="/freelances" element={<Freelances />} />
-                <Route path="*" element={<Error />} />
+   
+          <ThemeProvider>
+          <GlobalStyle />
 
-            </Routes>
-                  </Router>
+               <Header />
+               <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/survey" element={<Survey />} />
+                    <Route
+                         path="/survey/:questionNumber"
+                         element={<Survey />}
+                    />
+                    <Route path="/results" element={<Results />} />
+                    <Route path="/freelances" element={<Freelances />} />
+                    <Route path="*" element={<Error />} />
+               </Routes>
+               <Footer/>
+
+               </ThemeProvider>
+          </Router>
      </StrictMode>
-)
+);
--- src/pages/Survely/index.tsx
+++ src/pages/Survely/index.tsx
@@ -1,24 +1,94 @@
 
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import colors from '@/utils/style/color'
+import { Loader } from '@/utils/style/Atoms'
+import { useFetch } from '@/utils/hooks'
+
+
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+`
+
+const QuestionContent = styled.span`
+  margin: 30px;
+`
+
+const LinkWrapper = styled.div`
+  padding-top: 30px;
+  & a {
+    color: black;
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
+`
 
 function Survey() {
   const { questionNumber } = useParams()
   const questionNumberInt = parseInt(questionNumber)
   const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
   const nextQuestionNumber = questionNumberInt + 1
+/*   const [surveyData, setSurveyData] = useState({})
+  const [isDataLoading, setDataLoading] = useState(false) */
+/*   const [error, setError] = useState(false)
+ */
+/*   useEffect(() => {
+    async function fetchSurvey() {
+      setDataLoading(true)
+      try {
+        const response = await fetch(`http://localhost:8000/survey`)
+        const { surveyData } = await response.json()
+        setSurveyData(surveyData)
+      } catch (err) {
+        console.log('===== error =====', err)
+        setError(true)
+      } finally {
+        setDataLoading(false)
+      }
+    }
+    fetchSurvey()
+  }, [])
+ */
+
+ const { data, isLoading, error } = useFetch(`http://localhost:8000/survey`)
+
+const surveyData = data?.surveyData
+console.log(surveyData);
+
+if (error) {
+    return <span>Oups il y a eu un problème</span>
+  }
+
   return (
-    <div>
-      <h1>Questionnaire 🧮</h1>
-      <h2>Question {questionNumber}</h2>
-    <div className=''><Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-      {questionNumberInt === 10 ? (
-        <Link to="/results">Résultats</Link>
-      ) : (
-        <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
-      )}  
-    </div></div>
-  )
+     <SurveyContainer>
+       <QuestionTitle>Question {questionNumber}</QuestionTitle>
+       {isLoading ? (
+         <Loader />
+       ) : surveyData && surveyData[questionNumber] ? ( // Vérifier si surveyData et la question sont disponibles
+         <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
+       ) : (
+         <span>La question n'existe pas ou les données ne sont pas encore disponibles.</span>
+       )}
+       <LinkWrapper>
+         <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+         {surveyData && surveyData[questionNumberInt + 1] ? (
+           <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+         ) : (
+           <Link to="/results">Résultats</Link>
+         )}
+       </LinkWrapper>
+     </SurveyContainer>
+   )
+   
 }
 
 export default Survey
